f63d02ba3824c29b0bac3d326b4683c1a86daaff
Travis Cook <travismcook93@gmail.com>
2024-01-31 16:40:03 -0700

    added cors support?

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-  });
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://main.d24wadcr9d2smb.amplifyapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 module.exports = router;
