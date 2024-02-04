22b0bf1d1c502d19bee9162fc7863b61e3aeaa4f
Travis Cook <travismcook93@gmail.com>
2024-02-04 01:13:25 -0700

    added logic for handling all sports and cron job

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,17 +2,74 @@ require('dotenv').config()
 const connection = require('../config/connection');
 const { Odds } = require('../models');
 const axios = require('axios')
+const moment = require('moment')
+const now = moment()
 
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
     console.log("DB CONNECTED ---- STARTING SEED")
-    let sports = ["basketball_nba", "americanfootball_nfl", "icehockey_nhl", "soccer_epl"]
+    let sports = [{
+        name: "basketball_nba",
+        startMonth: 10,
+        endMonth: 4,
+        multiYear: true
+    },
+    {
+        name: "americanfootball_nfl",
+        startMonth: 9,
+        endMonth: 1,
+        multiYear: true
+    },
+    {
+        name: "icehockey_nhl",
+        startMonth: 10,
+        endMonth: 4,
+        multiYear: true
+    },
+    {
+        name: "soccer_epl",
+        startMonth: 8,
+        endMonth: 5,
+        multiYear: true
+    },
+    {
+        name: "baseball_mlb",
+        startMonth: 3,
+        endMonth: 10,
+        multiYear: false
+    },  {
+        name: "mma_mixed_martial_arts",
+        startMonth: 12,
+        endMonth: 12,
+        multiYear: false
+    }, {
+        name: "soccer_usa_mls",
+        startMonth: 2,
+        endMonth: 10,
+        multiYear: false
+    },
+]
+    let searchSports = []
     let events = []
+    sports.map((sport) => {
+      if(sport.endMonth < sport.startMonth){ //if true, sport has multi year season
+        if(now.month()+1 >= sport.startMonth || now.month()+1 <= sport.endMonth){
+            searchSports.push(sport)
+        }
+      }else if(sport.startMonth === sport.endMonth){ // if true, sport is year round
+        searchSports.push(sport)
+      }else{ // else case covers single year seasons
+        if(now.month()+1 <= sport.startMonth && now.month()+1 >= sport.endMonth){
+            searchSports.push(sport)
+        }
+      }
+    })
+
     await Odds.deleteMany()
     await axios.all(sports.map((sport) =>
-    axios.get(`https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${process.env.API_KEY}&regions=us&oddsFormat=american&markets=h2h,spreads,totals`)
+    axios.get(`https://api.the-odds-api.com/v4/sports/${sport.name}/odds/?apiKey=${process.env.API_KEY}&regions=us&oddsFormat=american&markets=h2h,spreads`)
     )).then(async (data) => {
         try {
             data.map(async (item) => {
--- server.js
+++ server.js
@@ -23,20 +23,74 @@ const dbCron =  () => {
     cron.schedule('* */24 * * *', async () => {
         console.log("CRON JOB REPEAT")
             console.log("DB CONNECTED ---- STARTING SEED")
-            let sports = ["basketball_nba", "americanfootball_nfl", "icehockey_nhl"]
+            let sports = [{
+                name: "basketball_nba",
+                startMonth: 10,
+                endMonth: 4,
+                multiYear: true
+            },
+            {
+                name: "americanfootball_nfl",
+                startMonth: 9,
+                endMonth: 1,
+                multiYear: true
+            },
+            {
+                name: "icehockey_nhl",
+                startMonth: 10,
+                endMonth: 4,
+                multiYear: true
+            },
+            {
+                name: "soccer_epl",
+                startMonth: 8,
+                endMonth: 5,
+                multiYear: true
+            },
+            {
+                name: "baseball_mlb",
+                startMonth: 3,
+                endMonth: 10,
+                multiYear: false
+            },  {
+                name: "mma_mixed_martial_arts",
+                startMonth: 12,
+                endMonth: 12,
+                multiYear: false
+            }, {
+                name: "soccer_usa_mls",
+                startMonth: 2,
+                endMonth: 10,
+                multiYear: false
+            },
+        ]
+            let searchSports = []
             let events = []
-            await Odds.deleteMany({})
+            sports.map((sport) => {
+              if(sport.endMonth < sport.startMonth){ //if true, sport has multi year season
+                if(now.month()+1 >= sport.startMonth || now.month()+1 <= sport.endMonth){
+                    searchSports.push(sport)
+                }
+              }else if(sport.startMonth === sport.endMonth){ // if true, sport is year round
+                searchSports.push(sport)
+              }else{ // else case covers single year seasons
+                if(now.month()+1 <= sport.startMonth && now.month()+1 >= sport.endMonth){
+                    searchSports.push(sport)
+                }
+              }
+            })
+        
+            await Odds.deleteMany()
             await axios.all(sports.map((sport) =>
-            axios.get(`https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${process.env.API_KEY}&regions=us&oddsFormat=american&markets=h2h,spreads,totals`)
+            axios.get(`https://api.the-odds-api.com/v4/sports/${sport.name}/odds/?apiKey=${process.env.API_KEY}&regions=us&oddsFormat=american&markets=h2h,spreads`)
             )).then(async (data) => {
                 try {
                     data.map(async (item) => {
-                        console.log("item.data: ")
                         item.data.map((event) => {
                             events.push(event)
                         })
                     })
-                    await Odds.collection.insertMany(events)
+                    await Odds.insertMany(events)
                     console.info('Odds Seeding complete! 🌱');
                 } catch (err) {
                     if (err) throw (err)
