e82e865b783cabc65b66c202e071bcfc3704c0e0
Travis Cook <traviscookdev@gmail.com>
2024-12-23 00:37:15 -0700

    updated way of calculating implied probability

--- seeds/seed.js
+++ seeds/seed.js
@@ -917,21 +917,39 @@ const dataSeed = async () => {
                     // Loop over all outcomes for each market
                     await Promise.all(market.outcomes.map(async (outcome) => {
                         // Perform the update using arrayFilters to target the correct outcome
-                        await Odds.findOneAndUpdate(
-                            { 'id': game.id }, // Filter by game id
-                            {
-                                $set: {
-                                    'bookmakers.$[bookmaker].markets.$[market].outcomes.$[outcome].impliedProb': 100 / (Math.abs(outcome.price) + 100)
+                        if(outcome.price < 0 ){
+                            await Odds.findOneAndUpdate(
+                                { 'id': game.id }, // Filter by game id
+                                {
+                                    $set: {
+                                        'bookmakers.$[bookmaker].markets.$[market].outcomes.$[outcome].impliedProb': Math.abs(outcome.price) / (Math.abs(outcome.price) + 100)
+                                    }
+                                },
+                                {
+                                    arrayFilters: [
+                                        { 'bookmaker.key': bookmaker.key }, // Match bookmaker by key
+                                        { 'market.key': market.key }, // Match market by key
+                                        { 'outcome._id': outcome._id } // Match outcome by its _id
+                                    ]
                                 }
-                            },
-                            {
-                                arrayFilters: [
-                                    { 'bookmaker.key': bookmaker.key }, // Match bookmaker by key
-                                    { 'market.key': market.key }, // Match market by key
-                                    { 'outcome._id': outcome._id } // Match outcome by its _id
-                                ]
-                            }
-                        );
+                            );
+                        }else {
+                            await Odds.findOneAndUpdate(
+                                { 'id': game.id }, // Filter by game id
+                                {
+                                    $set: {
+                                        'bookmakers.$[bookmaker].markets.$[market].outcomes.$[outcome].impliedProb': 100 / (outcome.price + 100)
+                                    }
+                                },
+                                {
+                                    arrayFilters: [
+                                        { 'bookmaker.key': bookmaker.key }, // Match bookmaker by key
+                                        { 'market.key': market.key }, // Match market by key
+                                        { 'outcome._id': outcome._id } // Match outcome by its _id
+                                    ]
+                                }
+                            ); 
+                        }
                     }));
                 }));
             }));
