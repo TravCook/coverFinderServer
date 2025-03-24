caba154c964baf396b51bcac12d86d96cbcb97a2
Travis Cook <traviscookdev@gmail.com>
2025-03-23 20:07:46 -0600

    freeing up more memory in processes

--- controllers/oddsController.js
+++ controllers/oddsController.js
@@ -157,18 +157,14 @@ module.exports = {
     },
 
     async getPastGames(req, res) {
-        console.log(`REQUEST RECIEVED @ ${moment().format('HH:mm:ss')}`)
         const twoWeeks = new Date();
         twoWeeks.setDate(twoWeeks.getDate() - 7);
         twoWeeks.setHours(0, 0, 0, 0);  // Set time to midnight
         try {
-            console.log(`fetching data @ ${moment().format('HH:mm:ss')}`)
             pastGames = await PastGameOdds.find({ predictedWinner: { $exists: true, $ne: null }, commence_time: { $gte: twoWeeks.toISOString(), $lt: new Date().toISOString() } }).select('-homeTeamStats -awayTeamStats').sort({ commence_time: -1});
-            console.log(`data fetched @ ${moment().format('HH:mm:ss')}`)
             data = {
                 pastGames: pastGames
             }
-            console.log(`RESPONSE SENT @ ${moment().format('HH:mm:ss')}`)
             return res.json(data);
 
         } catch (err) {
