e4ee32f626c848e2981c133458b86a488558ed2b
Travis Cook <traviscookdev@gmail.com>
2025-04-03 00:58:00 -0600

    retrying indexing with new amount of games

--- utils/helperFunctions/mlModelFuncs/indexHelpers.js
+++ utils/helperFunctions/mlModelFuncs/indexHelpers.js
@@ -296,7 +296,7 @@ const indexAdjuster = async (currentOdds, sport, allPastGames, weightArray, past
                 },
             },
             { $sort: { commence_time: -1 } }, // Sort by date to ensure most recent games come first
-            { $limit: 30 }, // Limit to the last 15 games
+            { $limit: sport.indexGames }, // Limit to the last 15 games
             { $sort: { highestIndex: -1 } }, // sort by the highest index
             // { $limit: 1 }, // limit to just one result
         ]).exec()
@@ -318,11 +318,11 @@ const indexAdjuster = async (currentOdds, sport, allPastGames, weightArray, past
                 },
             },
             { $sort: { commence_time: -1 } }, // Sort by date to ensure most recent games come first
-            { $limit: 30 }, // Limit to the last 15 games
+            { $limit: sport.indexGames }, // Limit to the last 15 games
             { $sort: { lowestIndex: 1 } }, // sort by the highest index
             // { $limit: 1 }, // limit to just one result
         ]).exec()
-
+        console.log(sport.indexGames)
         if (!maxGame || maxGame.length === 0) {
             // If no result for maxGame (edge case), fallback to the first 10 games
             maxGame = await PastGameOdds.aggregate([
