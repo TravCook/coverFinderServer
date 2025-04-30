2b8e4135e899e2a95ca97ce76d9e9d5701ed031b
Travis Cook <traviscookdev@gmail.com>
2025-04-29 21:08:06 -0600

    changed logic for saving stats to teams to lighten load on api server

--- utils/helperFunctions/dataHelpers/removeHelper.js
+++ utils/helperFunctions/dataHelpers/removeHelper.js
@@ -172,14 +172,15 @@ const removePastGames = async (currentOdds) => {
 
 
                             }
-                        } else if (event.competitions[0].status.type.description === 'Postponed') {
-                            // Delete the game from the Odds collection
-                            let deletedGame = await Odds.findOneAndDelete({ _id: game._doc._id });
-                            if (deletedGame) {
-                                console.log(`deleted game: ${deletedGame.home_team} vs ${deletedGame.away_team}`);
-                            }
-                            deletedGame = null
                         }
+                        //  else if (event.competitions[0].status.type.description === 'Postponed') {
+                        //     // Delete the game from the Odds collection
+                        //     let deletedGame = await Odds.findOneAndDelete({ _id: game._doc._id });
+                        //     if (deletedGame) {
+                        //         console.log(`deleted game: ${deletedGame.home_team} vs ${deletedGame.away_team}`);
+                        //     }
+                        //     deletedGame = null
+                        // }
                     } else {
                         // Delete the game from the Odds collection
                         let deletedGame = await Odds.findOneAndDelete({ _id: game._doc._id });
