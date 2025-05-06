eec7720558e8f849269d32006f4fed3e8f158279
Travis Cook <traviscookdev@gmail.com>
2025-05-06 11:16:44 -0600

    changed logic of cutoff normalization

--- utils/helperFunctions/mlModelFuncs/indexHelpers.js
+++ utils/helperFunctions/mlModelFuncs/indexHelpers.js
@@ -269,7 +269,7 @@ const indexAdjuster = async (currentOdds, initalsport, allPastGames, weightArray
     console.log(`STARTING INDEXING FOR ${initalsport.name} @ ${moment().format('HH:mm:ss')}`);
     const currentDate = new Date();
     const oneYearAgo = new Date(currentDate);
-    oneYearAgo.setDate(currentDate.getDate() - 182); // Subtract 365 days
+    oneYearAgo.setDate(currentDate.getDate() - 90); // Subtract 365 days
     oneYearAgo.setHours(0, 0, 0, 0);  // Set time to midnight
     let sport = await Sport.findOne({ name: initalsport.name }).exec()
 
@@ -501,6 +501,7 @@ const indexAdjuster = async (currentOdds, initalsport, allPastGames, weightArray
             }
         }
     }
+    extremes = null
     console.log('Normalized indexes found and applied')
     console.log(`FINSHED INDEXING FOR ${initalsport.name} @ ${moment().format('HH:mm:ss')}`);
 }
