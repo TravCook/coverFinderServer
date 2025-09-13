4450c7b0beaeafee3a376604fb0d51991aa2e78d
Travis Cook <traviscookdev@gmail.com>
2025-09-13 16:06:24 -0600

    more oddsSeed testing

--- utils/helperFunctions/dataHelpers/databaseSavers.js
+++ utils/helperFunctions/dataHelpers/databaseSavers.js
@@ -105,16 +105,16 @@ const statDBSaver = async (game, team, sport, gameSQL, homeAway) => {
         }
     } else if (gameSQL.probablePitcher === null && sport.name === 'baseball_mlb') {
         let allPitchers = Object.values(team.pitcherStats)
-        console.log(allPitchers)
+        console.log(team.pitcherStats)
         if (allPitchers.length > 0) {
-            console.log(team.pitcherStats[allPitchers[0]])
+            // console.log(team.pitcherStats[allPitchers[0]])
             for (const stat in team.pitcherStats[allPitchers[0]]) {
                 let statAverage
                 for (const pitcher in allPitchers) {
                     statAverage += (allPitchers[pitcher][stat])
                 }
                 statAverage = statAverage / allPitchers.length
-                console.log(stat, statAverage)
+                // console.log(stat, statAverage)
                 if (stat !== 'BSBsaves' && stat !== 'BSBsavePct' && stat !== 'BSBshutouts') teamStats[stat] = statAverage
             }
         }
