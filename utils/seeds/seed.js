854226199eeb8c1fa2ec641ff7c363595dcfe053
Travis Cook <traviscookdev@gmail.com>
2025-10-03 15:58:05 -0600

    added logs in new places to track ram usage

--- utils/seeds/seed.js
+++ utils/seeds/seed.js
@@ -87,7 +87,7 @@ const mlModelTrainSeed = async () => {
     for (let sport of sports) {
         let inSeason = isSportInSeason(sport)
         // Multi-year sports (e.g., NFL, NBA, NHL, etc.)
-        if (inSeason) {
+        if (inSeason && (sport.name !== 'americanfootball_ncaaf' || sport.name !== 'americanfootball_nfl')) {
             let upcomingGames = odds.filter((game) => game.sport_key === sport.name)
             let pastGames = await db.Games.findAll({
                 where: { complete: true, sport_key: sport.name },
