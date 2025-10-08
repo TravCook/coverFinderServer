4f640dd6e8b0d9196a97b64cca0afb344d1448ab
Travis Cook <traviscookdev@gmail.com>
2025-10-07 18:34:47 -0600

    after losing tigers playoff game, changing logic to log out instead of destroying, too unreliable still

--- utils/helperFunctions/dataHelpers/removeHelper.js
+++ utils/helperFunctions/dataHelpers/removeHelper.js
@@ -95,12 +95,15 @@ const removePastGames = async (currentOdds) => {
                             }
                         }
                         else if (event.competitions[0].status.type.description === 'Postponed') {
-                            await db.Games.destroy({
-                                where: {
-                                    id: game.id
-                                },
+                            // await db.Games.destroy({
+                            //     where: {
+                            //         id: game.id
+                            //     },
 
-                            })
+                            // })
+                            console.log('Game Postponed:', game.id);
+                            console.log(`${game['awayTeamDetails.espnDisplayName']} at ${game['homeTeamDetails.espnDisplayName']}`)
+                            console.log(`https://site.api.espn.com/apis/site/v2/sports/${game.sport_title}/${game['homeTeamDetails.espnLeague']}/teams/${game['homeTeamDetails.espnID']}/schedule`)
                         }
                         // else if () {
                         //     await db.Games.update({
@@ -115,12 +118,15 @@ const removePastGames = async (currentOdds) => {
                         // }
                     }
                     else {
-                        await db.Games.destroy({
-                            where: {
-                                id: game.id
-                            },
+                        // await db.Games.destroy({
+                        //     where: {
+                        //         id: game.id
+                        //     },
 
-                        })
+                        // })
+                        console.log ('No matching event found for game:', game.id);
+                        console.log(`${game['awayTeamDetails.espnDisplayName']} at ${game['homeTeamDetails.espnDisplayName']}`)
+                        console.log(`https://site.api.espn.com/apis/site/v2/sports/${game.sport_title}/${game['homeTeamDetails.espnLeague']}/teams/${game['homeTeamDetails.espnID']}/schedule`)
                     }
                 } catch (err) {
                     console.log(game)
